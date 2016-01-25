d540ccaaf6652f791496a93a69a805c9c1bdce97
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-01-25 11:23:24 -0300

    LPS-61692 Update package references

--- modules/apps/workflow/workflow-kaleo-forms-service/src/main/java/com/liferay/workflow/kaleo/forms/service/permission/KaleoProcessPermission.java
+++ modules/apps/workflow/workflow-kaleo-forms-service/src/main/java/com/liferay/workflow/kaleo/forms/service/permission/KaleoProcessPermission.java
@@ -15,8 +15,8 @@
 package com.liferay.workflow.kaleo.forms.service.permission;
 
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.security.auth.PrincipalException;
-import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.workflow.kaleo.forms.model.KaleoProcess;
 import com.liferay.workflow.kaleo.forms.service.KaleoProcessLocalServiceUtil;
 
