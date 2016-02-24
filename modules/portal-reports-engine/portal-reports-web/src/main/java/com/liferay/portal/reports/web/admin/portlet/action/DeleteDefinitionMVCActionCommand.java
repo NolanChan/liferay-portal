791d3ab87037824feed6b07733d267ae460c201c
Prathima Shreenath <prathima.shreenath@liferay.com>
2016-02-24 11:18:18 -0600

    LPS-63383 : Move portlet to portal-reports-web

---
+++ modules/portal-reports-engine/portal-reports-web/src/main/java/com/liferay/portal/reports/web/admin/portlet/action/DeleteDefinitionMVCActionCommand.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.reports.admin.portlet.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.reports.service.DefinitionServiceUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+/**
+ * @author Michael C. Han
+ */
+public class DeleteDefinitionMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long definitionId = ParamUtil.getLong(actionRequest, "definitionId");
+
+		DefinitionServiceUtil.deleteDefinition(definitionId);
+	}
+
+}
