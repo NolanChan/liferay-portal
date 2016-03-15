504997fce47bbf85f9c92f1506b9d1fd2bd40c07
briangreenwald <brian.greenwald@liferay.com>
2016-03-15 16:47:31 -0500

    LPS-63382 Update Jasper Engine to publish message listeners

--- modules/ee/apps/forms-and-workflow/portal-reports-engine/portal-reports-engine-jasper/src/main/java/com/liferay/portal/reports/engine/jasper/ReportEngineImpl.java
+++ modules/ee/apps/forms-and-workflow/portal-reports-engine/portal-reports-engine-jasper/src/main/java/com/liferay/portal/reports/engine/jasper/ReportEngineImpl.java
@@ -14,6 +14,17 @@
 
 package com.liferay.portal.reports.engine.jasper;
 
+import com.liferay.portal.kernel.concurrent.CallerRunsPolicy;
+import com.liferay.portal.kernel.concurrent.RejectedExecutionHandler;
+import com.liferay.portal.kernel.concurrent.ThreadPoolExecutor;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
+import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.reports.engine.ByteArrayReportResultContainer;
 import com.liferay.portal.reports.engine.ReportEngine;
 import com.liferay.portal.reports.engine.ReportFormatExporter;
 import com.liferay.portal.reports.engine.ReportFormatExporterRegistry;
@@ -24,20 +35,34 @@ import com.liferay.portal.reports.engine.ReportResultContainer;
 import com.liferay.portal.reports.engine.jasper.compiler.ReportCompiler;
 import com.liferay.portal.reports.engine.jasper.fillmanager.ReportFillManager;
 import com.liferay.portal.reports.engine.jasper.fillmanager.ReportFillManagerRegistry;
+import com.liferay.portal.reports.engine.messaging.DestinationNames;
+import com.liferay.portal.reports.engine.messaging.ReportCompilerRequestMessageListener;
+import com.liferay.portal.reports.engine.messaging.ReportRequestMessageListener;
 
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.ServletContext;
 
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Michael C. Han
  * @author Brian Wing Shun Chan
+ * @author Brian Greenwald
  */
 @Component(immediate = true, service = ReportEngine.class)
 public class ReportEngineImpl implements ReportEngine {
@@ -104,29 +129,149 @@ public class ReportEngineImpl implements ReportEngine {
 		_engineParameters = engineParameters;
 	}
 
-	@Reference(unbind = "-")
-	public void setReportCompiler(ReportCompiler reportCompiler) {
-		_reportCompiler = reportCompiler;
+	@Activate
+	protected void activate(ComponentContext componentContext) {
+		_bundleContext = componentContext.getBundleContext();
+
+		MessageListener reportCompilerRequestMessageListener =
+			new ReportCompilerRequestMessageListener(
+				this, new ByteArrayReportResultContainer());
+
+		_messageListeners.put(
+			DestinationNames.REPORT_COMPILER,
+			reportCompilerRequestMessageListener);
+
+		MessageListener reportRequestMessageListener =
+			new ReportRequestMessageListener(
+				this, new ByteArrayReportResultContainer());
+
+		_messageListeners.put(
+			DestinationNames.REPORT_REQUEST, reportRequestMessageListener);
+
+		for (String destinationName : _messageListeners.keySet()) {
+			registerDestination(destinationName);
+		}
 	}
 
-	@Reference(unbind = "-")
-	public void setReportFillManagerRegistry(
-		ReportFillManagerRegistry reportFillManagerRegistry) {
+	@Deactivate
+	protected void deactivate() {
+		if (_destinationServiceRegistrations != null) {
+			for (
+				ServiceRegistration<Destination>
+					destinationServiceRegistration :
+						_destinationServiceRegistrations) {
 
-		_reportFillManagerRegistry = reportFillManagerRegistry;
+				Destination destination = _bundleContext.getService(
+					destinationServiceRegistration.getReference());
+
+				destinationServiceRegistration.unregister();
+
+				destination.destroy();
+			}
+		}
+
+		if (_messageListenerServiceRegistrations != null) {
+			for (
+				ServiceRegistration<MessageListener>
+					messageListenerServiceRegistration :
+						_messageListenerServiceRegistrations) {
+
+				messageListenerServiceRegistration.unregister();
+			}
+		}
+
+		_bundleContext = null;
 	}
 
-	@Override
-	@Reference(unbind = "-")
-	public void setReportFormatExporterRegistry(
-		ReportFormatExporterRegistry reportFormatExporterRegistry) {
+	@Modified
+	protected void modified(ComponentContext componentContext) {
+		deactivate();
 
-		_reportFormatExporterRegistry = reportFormatExporterRegistry;
+		activate(componentContext);
 	}
 
+	protected void registerDestination(String destinationName) {
+		DestinationConfiguration destinationConfiguration =
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_PARALLEL,
+				destinationName);
+
+		destinationConfiguration.setMaximumQueueSize(_MAXIMUM_QUEUE_SIZE);
+
+		RejectedExecutionHandler rejectedExecutionHandler =
+			new CallerRunsPolicy() {
+
+				@Override
+				public void rejectedExecution(
+					Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							"The current thread will handle the request " +
+								"because the graph walker's task queue is at " +
+									"its maximum capacity");
+					}
+
+					super.rejectedExecution(runnable, threadPoolExecutor);
+				}
+
+			};
+
+		destinationConfiguration.setRejectedExecutionHandler(
+			rejectedExecutionHandler);
+
+		Destination destination = _destinationFactory.createDestination(
+			destinationConfiguration);
+
+		Dictionary<String, Object> destinationProperties =
+			new HashMapDictionary<>();
+
+		destinationProperties.put("destination.name", destination.getName());
+
+		ServiceRegistration<Destination> destinationServiceRegistration =
+			_bundleContext.registerService(
+				Destination.class, destination, destinationProperties);
+
+		_destinationServiceRegistrations.add(destinationServiceRegistration);
+
+		MessageListener messageListener = _messageListeners.get(
+			destinationName);
+
+		destination.register(messageListener);
+
+		ServiceRegistration<MessageListener>
+			messageListenerServiceRegistration = _bundleContext.registerService(
+				MessageListener.class, messageListener, destinationProperties);
+
+		_messageListenerServiceRegistrations.add(
+			messageListenerServiceRegistration);
+	}
+
+	private static final int _MAXIMUM_QUEUE_SIZE = 200;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ReportEngineImpl.class);
+
+	private BundleContext _bundleContext;
+
+	@Reference
+	private DestinationFactory _destinationFactory;
+
+	private final List<ServiceRegistration<Destination>>
+		_destinationServiceRegistrations = new ArrayList<>();
 	private Map<String, String> _engineParameters;
+	private final Map<String, MessageListener> _messageListeners =
+		new ConcurrentHashMap<>();
+	private final List<ServiceRegistration<MessageListener>>
+		_messageListenerServiceRegistrations = new ArrayList<>();
+
+	@Reference
 	private ReportCompiler _reportCompiler;
+
+	@Reference
 	private ReportFillManagerRegistry _reportFillManagerRegistry;
+
+	@Reference
 	private ReportFormatExporterRegistry _reportFormatExporterRegistry;
 
 }
