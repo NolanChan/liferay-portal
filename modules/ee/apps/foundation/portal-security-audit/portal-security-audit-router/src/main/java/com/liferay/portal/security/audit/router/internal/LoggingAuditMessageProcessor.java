47eea9b8c558bd99a24900500d427ae7c63472fe
Brian Chan <brian.chan@liferay.com>
2016-03-16 21:38:23 -0700

    LPS-62774 This got merged with portal-configuration-metatype

--- modules/ee/apps/foundation/portal-security-audit/portal-security-audit-router/src/main/java/com/liferay/portal/security/audit/router/internal/LoggingAuditMessageProcessor.java
+++ modules/ee/apps/foundation/portal-security-audit/portal-security-audit-router/src/main/java/com/liferay/portal/security/audit/router/internal/LoggingAuditMessageProcessor.java
@@ -14,7 +14,7 @@
 
 package com.liferay.portal.security.audit.router.internal;
 
-import com.liferay.bnd.util.ConfigurableUtil;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.audit.AuditMessage;
 import com.liferay.portal.kernel.concurrent.ConcurrentIdentityHashMap;
 import com.liferay.portal.kernel.log.Log;
