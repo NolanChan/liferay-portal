199ae476897bbdbdbfa0bb49cb3dc9951e5cbe59
Inacio Nery <inacio.nery@liferay.com>
2016-03-31 09:08:33 -0300

    LPS-64129 Add KaleoProcess Comparator

---
+++ modules/ee/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-forms-api/src/main/java/com/liferay/portal/workflow/kaleo/forms/comparator/KaleoProcessModifiedDateComparator.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.workflow.kaleo.forms.comparator;
+
+import com.liferay.exportimport.kernel.lar.StagedModelModifiedDateComparator;
+import com.liferay.portal.workflow.kaleo.forms.model.KaleoProcess;
+
+/**
+ * @author Inácio Nery
+ */
+public class KaleoProcessModifiedDateComparator
+	extends StagedModelModifiedDateComparator<KaleoProcess> {
+
+	public KaleoProcessModifiedDateComparator() {
+		this(false);
+	}
+
+	public KaleoProcessModifiedDateComparator(boolean ascending) {
+		super(ascending);
+	}
+
+	@Override
+	public String getOrderBy() {
+		return "KaleoProcess." + super.getOrderBy();
+	}
+
+}
