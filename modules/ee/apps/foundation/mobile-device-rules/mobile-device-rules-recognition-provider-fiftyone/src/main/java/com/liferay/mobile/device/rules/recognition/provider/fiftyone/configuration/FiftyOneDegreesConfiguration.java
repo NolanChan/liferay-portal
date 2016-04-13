d41f3d5937e0fcb742b3345e5902d7c3af4e229b
Prathima Shreenath <prathima.shreenath@liferay.com>
2016-04-13 13:37:11 -0500

    LPS-65030 Add 51Degrees configuration & dataSet

---
+++ modules/ee/apps/foundation/mobile-device-rules/mobile-device-rules-recognition-provider-fiftyone/src/main/java/com/liferay/mobile/device/rules/recognition/provider/fiftyone/configuration/FiftyOneDegreesConfiguration.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.mobile.device.rules.recognition.provider.fiftyone.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Brian Greenwald
+ * @author Prathima Shreenath
+ */
+@ExtendedObjectClassDefinition(category = "foundation")
+@Meta.OCD(
+	id = "com.liferay.mobile.device.rules.recognition.provider.fiftyone.configuration.FiftyOneDegreesConfiguration",
+	localization = "content/Language",
+	name = "51.degrees.device.detection.configuration.name"
+)
+public interface FiftyOneDegreesConfiguration {
+
+	@Meta.AD(
+		deflt = "/META-INF/resources/51Degrees-PremiumV3_2.dat.gz",
+		required = false
+	)
+	public String fiftyOneDegreesDatabaseFileName();
+
+}
