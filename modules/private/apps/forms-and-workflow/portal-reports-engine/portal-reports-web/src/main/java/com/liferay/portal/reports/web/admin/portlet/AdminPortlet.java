d224f038ac0d0420f43edd3f3fc0868ed833430c
Prathima Shreenath <prathima.shreenath@liferay.com>
2016-04-15 15:17:12 -0500

    LPS-63383 : move from ee to private

--- modules/private/apps/forms-and-workflow/portal-reports-engine/portal-reports-web/src/main/java/com/liferay/portal/reports/web/admin/portlet/AdminPortlet.java
+++ modules/private/apps/forms-and-workflow/portal-reports-engine/portal-reports-web/src/main/java/com/liferay/portal/reports/web/admin/portlet/AdminPortlet.java
@@ -64,7 +64,7 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.portlet-info.short-title=Reports Admin",
 		"javax.portlet.portlet-info.title=Reports Admin",
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
 		"javax.portlet.supports.mime-type=text/html"
 	},
 	service = Portlet.class
