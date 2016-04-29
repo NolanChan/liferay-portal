4a43b193fc6fa201ab85ab3c5d605ab727630503
Michael Han <michael.han@liferay.com>
2016-04-28 20:22:05 -0500

    LPS-63383 put constants in proper class

--- modules/private/apps/forms-and-workflow/portal-reports-engine/portal-reports-engine-console-api/src/main/java/com/liferay/portal/reports/engine/console/constants/ReportsEngineConsoleConstants.java
+++ modules/private/apps/forms-and-workflow/portal-reports-engine/portal-reports-engine-console-api/src/main/java/com/liferay/portal/reports/engine/console/constants/ReportsEngineConsoleConstants.java
@@ -15,15 +15,9 @@
 package com.liferay.portal.reports.engine.console.constants;
 
 /**
- * @author Brian Wing Shun Chan
+ * @author Michael C. Han
  */
-public class ReportsPortletKeys {
-
-	public static final String DISPLAY_REPORTS =
-		"com_liferay_portal_reports_engine_console_web_display_portlet_DisplayPortlet";
-
-	public static final String REPORTS_ADMIN =
-		"com_liferay_portal_reports_engine_console_web_admin_portlet_AdminPortlet";
+public class ReportsEngineConsoleConstants {
 
 	public static final String SERVICE_NAME = "com.liferay.portal.reports";
 
