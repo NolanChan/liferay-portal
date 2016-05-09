bab349c4eeba23424295f62d50e2b65b976e8249
Inacio Nery <inacio.nery@liferay.com>
2016-05-09 12:42:48 -0300

    LPS-65628 Add upgrade to remove references from kaleo forms

--- modules/private/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-forms-web/src/main/java/com/liferay/portal/workflow/kaleo/forms/web/upgrade/v1_0_2/UpgradePortletId.java
+++ modules/private/apps/forms-and-workflow/portal-workflow/portal-workflow-kaleo-forms-web/src/main/java/com/liferay/portal/workflow/kaleo/forms/web/upgrade/v1_0_2/UpgradePortletId.java
@@ -14,13 +14,70 @@
 
 package com.liferay.portal.workflow.kaleo.forms.web.upgrade.v1_0_2;
 
+import com.liferay.portal.kernel.model.LayoutTypePortletConstants;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.workflow.kaleo.forms.web.constants.KaleoFormsPortletKeys;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * @author Inácio Nery
  */
 public class UpgradePortletId
 	extends com.liferay.portal.upgrade.util.UpgradePortletId {
+
+	protected void deletePortletReferences(String portletId) throws Exception {
+		runSQL("delete from Portlet where portletId = '" + portletId + "'");
+
+		runSQL(
+			"delete from PortletPreferences where portletId = '" + portletId +
+				"'");
+
+		runSQL("delete from ResourceAction where name = '" + portletId + "'");
+
+		runSQL(
+			"delete from ResourcePermission where name = '" + portletId + "'");
+
+		removePortletFromLayouts(portletId);
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		super.doUpgrade();
+
+		deletePortletReferences("1_WAR_kaleoformsportlet");
+	}
+
+	protected String getNewTypeSettings(
+		String typeSettings, String oldRootPortletId) {
+
+		UnicodeProperties typeSettingsProperties = new UnicodeProperties(true);
+
+		typeSettingsProperties.fastLoad(typeSettings);
+
+		for (int i = 1; i <= 10; i++) {
+			String column = LayoutTypePortletConstants.COLUMN_PREFIX + i;
+
+			if (!typeSettingsProperties.containsKey(column)) {
+				continue;
+			}
+
+			String[] portletIds = StringUtil.split(
+				typeSettingsProperties.getProperty(column));
+
+			portletIds = ArrayUtil.remove(portletIds, oldRootPortletId);
+
+			typeSettingsProperties.setProperty(
+				column, StringUtil.merge(portletIds).concat(StringPool.COMMA));
+		}
+
+		return typeSettingsProperties.toString();
+	}
 
 	@Override
 	protected String[][] getRenamePortletIdsArray() {
@@ -30,6 +87,30 @@ public class UpgradePortletId
 				KaleoFormsPortletKeys.KALEO_FORMS_ADMIN
 			}
 		};
+	}
+
+	protected void removePortletFromLayouts(String oldRootPortletId)
+		throws Exception {
+
+		StringBundler sb = new StringBundler(2);
+
+		sb.append("select plid, typeSettings from Layout where ");
+
+		sb.append(getTypeSettingsCriteria(oldRootPortletId));
+
+		try (PreparedStatement ps = connection.prepareStatement(sb.toString());
+			ResultSet rs = ps.executeQuery()) {
+
+			while (rs.next()) {
+				long plid = rs.getLong("plid");
+				String typeSettings = rs.getString("typeSettings");
+
+				String newTypeSettings = getNewTypeSettings(
+					typeSettings, oldRootPortletId);
+
+				updateLayout(plid, newTypeSettings);
+			}
+		}
 	}
 
 }
