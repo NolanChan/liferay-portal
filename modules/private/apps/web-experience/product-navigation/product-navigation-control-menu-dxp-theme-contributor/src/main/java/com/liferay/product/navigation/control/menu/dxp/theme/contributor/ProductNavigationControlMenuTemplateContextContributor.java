bac895836a35ff58704271aba7994c64185799e1
Marcos <marcos.castro@liferay.com>
2016-05-10 12:56:03 +0200

    LPS-65425 - Java packages re-naming

--- modules/private/apps/web-experience/product-navigation/product-navigation-control-menu-dxp-theme-contributor/src/main/java/com/liferay/product/navigation/control/menu/dxp/theme/contributor/ProductNavigationControlMenuTemplateContextContributor.java
+++ modules/private/apps/web-experience/product-navigation/product-navigation-control-menu-dxp-theme-contributor/src/main/java/com/liferay/product/navigation/control/menu/dxp/theme/contributor/ProductNavigationControlMenuTemplateContextContributor.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.product.navigation.control.menu.theme.contributor;
+package com.liferay.product.navigation.control.menu.theme.contributor.dxp;
 
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.template.TemplateContextContributor;
