62b1b6000e6e2e639a2abd7d5dcbcefb48f7b198
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2016-05-17 10:30:00 -0700

    LPS-65425 Fix wrong package

--- modules/private/apps/web-experience/product-navigation/product-navigation-control-menu-dxp-theme-contributor/src/main/java/com/liferay/product/navigation/control/menu/dxp/theme/contributor/ProductNavigationControlMenuTemplateContextContributor.java
+++ modules/private/apps/web-experience/product-navigation/product-navigation-control-menu-dxp-theme-contributor/src/main/java/com/liferay/product/navigation/control/menu/dxp/theme/contributor/ProductNavigationControlMenuTemplateContextContributor.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.product.navigation.control.menu.theme.contributor.dxp;
+package com.liferay.product.navigation.control.menu.dxp.theme.contributor;
 
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.template.TemplateContextContributor;
