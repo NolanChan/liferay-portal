8e00168c7ea9dbc4520c2055d26cf3600cc21d96
shuyangzhou <shuyang.zhou@liferay.com>
2016-05-18 11:45:32 -0700

    LPS-65950 Create portal-properties-swapper module to change default company name and logo for DXP platform.

---
+++ modules/private/apps/foundation/portal/portal-properties-swapper/src/main/java/com/liferay/portal/properties/swapper/internal/DefaultCompanyLogoSwapper.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.properties.swapper.internal;
+
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.util.PropsUtil;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Shuyang Zhou
+ */
+@Component(enabled = false, immediate = true)
+public class DefaultCompanyLogoSwapper {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Bundle bundle = bundleContext.getBundle();
+
+		String bundleIdString = String.valueOf(bundle.getBundleId());
+
+		PropsUtil.set(
+			PropsKeys.IMAGE_DEFAULT_COMPANY_LOGO,
+			bundleIdString +
+				";com/liferay/portal/properties/swapper/internal/" +
+					"company_dxp_logo.png");
+	}
+
+}
