0d8ccb986571f85de2d5cfa8fe1193e1c5b10483
Carlos Sierra <carlos.sierra@liferay.com>
2016-07-07 17:04:20 +0200

    LPS-66389 Panel App for portlet registration

---
+++ modules/private/apps/saml/saml-web/src/main/java/com/liferay/saml/web/admin/AdminPortletPanelApp.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.saml.web.admin;
+
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.model.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.category.key=" + PanelCategoryKeys.CONTROL_PANEL_CONFIGURATION
+	},
+	service = PanelApp.class
+)
+public class AdminPortletPanelApp extends BasePanelApp {
+
+	public String getPortletId() {
+		return "com_liferay_saml_admin_portlet_AdminPortlet";
+	}
+
+	@Override
+	@Reference(
+		target = "(javax.portlet.name=com_liferay_saml_admin_portlet_AdminPortlet)",
+		unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
+
+}
