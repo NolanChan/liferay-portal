0b5b13c5aa3cca102a4968ba8c448907e1c51d35
Jorge Díaz <jorge.diaz@liferay.com>
2016-09-06 12:38:12 +0200

    LPS-66133 Add post upgrade module to fix the issue

---
+++ modules/private/post-upgrade-fix/post-upgrade-fix-LPS_66133/src/main/java/com/liferay/post/upgrade/fix/LPS_66133/PostUpgradeFixOSGiCommands.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.post.upgrade.fix.LPS_66133;
+
+import com.liferay.message.boards.kernel.model.MBCategoryConstants;
+import com.liferay.message.boards.kernel.model.MBDiscussion;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+
+import java.io.IOException;
+
+import java.sql.SQLException;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * <p>
+ * Post upgrade fix for https://issues.liferay.com/browse/LPS-66133
+ * </p>
+ *
+ * @author Jorge Díaz
+ */
+@Component(
+	immediate = true,
+	property = {
+		"osgi.command.function=LPS_66133", "osgi.command.scope=postUpgradeFix"
+	},
+	service = PostUpgradeFixOSGiCommands.class
+)
+public class PostUpgradeFixOSGiCommands {
+
+	public void LPS_66133() {
+		System.out.println("Executing postUpgradeFix:LPS_66133");
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Executing postUpgradeFix:LPS_66133");
+		}
+
+		try {
+			long classNameId = PortalUtil.getClassNameId(
+				MBDiscussion.class.getName());
+
+			StringBundler sb = new StringBundler(6);
+
+			sb.append("delete from AssetEntry where classPK in (");
+			sb.append("select messageId from MBMessage where threadId in (");
+			sb.append("select threadId from MBThread where categoryId = ");
+			sb.append(MBCategoryConstants.DISCUSSION_CATEGORY_ID);
+			sb.append(" and messagecount = 1)) and classNameId = ");
+			sb.append(classNameId);
+
+			runSQL(sb.toString());
+
+			runSQL(
+				"delete from MBMessage where threadId in (" +
+					"select threadId from MBThread where categoryId = " +
+						MBCategoryConstants.DISCUSSION_CATEGORY_ID +
+							" and messagecount = 1)");
+
+			runSQL(
+				"delete from MBDiscussion where threadId in (" +
+					"select threadId from MBThread where categoryId = " +
+						MBCategoryConstants.DISCUSSION_CATEGORY_ID +
+							" and messagecount = 1)");
+
+			runSQL(
+				"delete from MBThread where categoryId = " +
+					MBCategoryConstants.DISCUSSION_CATEGORY_ID +
+						" and messagecount = 1");
+
+			System.out.println("postUpgradeFix:LPS_66133 execution finished");
+
+			if (_log.isInfoEnabled()) {
+				_log.info("postUpgradeFix:LPS_66133 execution finished");
+			}
+		}
+		catch (Exception e) {
+			System.err.println(
+				"An exception occurred during postUpgradeFix:LPS_66133 " +
+					"execution, see log for more information");
+
+			_log.error(
+				"An exception occurred during postUpgradeFix:LPS_66133 " +
+					"execution",
+				e);
+		}
+	}
+
+	public void runSQL(String template) throws IOException, SQLException {
+		DB db = DBManagerUtil.getDB();
+
+		db.runSQL(template);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PostUpgradeFixOSGiCommands.class);
+
+}
