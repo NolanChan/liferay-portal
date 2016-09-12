e80ceca6f5e8db8eb17b944d0ac587e17fdf9498
Igor Beslic <igor.beslic@liferay.com>
2016-09-12 14:49:15 +0200

    CLDSVCS-2356 osb-lcs-portlet - Add OSBLCSConstants

---
+++ modules/private/apps/osb-lcs/osb-lcs-portlet/osb-lcs-portlet-web/src/main/java/com/liferay/osb/lcs/constants/OSBLCSConstants.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.lcs.constants;
+
+/**
+ * @author Igor Beslic
+ */
+public class OSBLCSConstants {
+
+	public static final String PUBLIC_PATH_CHECK_LCS_HEALTH =
+		"/portal/lcs/check_lcs_health";
+
+	public static final String PUBLIC_PATH_CHECK_LCS_INVITATION =
+		"/portal/lcs/check_lcs_invitation";
+
+	public static final String PUBLIC_PATH_PATCH_UPLOAD =
+		"/portal/lcs/patch/upload";
+
+	public static final String PUBLIC_PATH_UPLOAD_LCS_CLIENT_EXCEPTION =
+		"/portal/lcs/upload_lcs_client_exception";
+
+}
