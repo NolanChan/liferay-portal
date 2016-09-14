fec377b7359399db27322c3c79cf643e569ef752
Matija Petanjek <matija.petanjek@liferay.com>
2016-09-14 12:21:27 +0200

    CLDSVCS-2161 osb-lcs-portlet - update OSBLCSPortletKeys

--- modules/private/apps/osb-lcs/osb-lcs-portlet/osb-lcs-portlet-api/src/main/java/com/liferay/osb/lcs/constants/OSBLCSPortletKeys.java
+++ modules/private/apps/osb-lcs/osb-lcs-portlet/osb-lcs-portlet-api/src/main/java/com/liferay/osb/lcs/constants/OSBLCSPortletKeys.java
@@ -18,31 +18,53 @@ package com.liferay.osb.lcs.constants;
  * @author Peter Shin
  * @author Igor Beslic
  * @author Marko Cikos
+ * @author Matija Petanjek
  */
 public class OSBLCSPortletKeys {
 
-	public static final String ACCOUNT = "12_WAR_osblcsportlet";
+	public static final String ACCOUNT =
+		"com_liferay_osb_lcs_account_portlet_AccountPortlet";
 
-	public static final String ADMIN = "1_WAR_osblcsportlet";
+	public static final String ADMIN =
+		"com_liferay_osb_lcs_admin_portlet_AdminPortlet";
 
-	public static final String BREADCRUMB = "2_WAR_osblcsportlet";
+	public static final String BREADCRUMB =
+		"com_liferay_osb_lcs_breadcrumb_portlet_BreadcrumbPortlet";
 
-	public static final String DOWNLOADS = "8_WAR_osblcsportlet";
+	public static final String DASHBOARD =
+		"com_liferay_osb_lcs_dashboard_portlet_DashboardPortlet";
 
-	public static final String ENROLLMENT = "6_WAR_osblcsportlet";
+	public static final String DOWNLOADS =
+		"com_liferay_osb_lcs_downloads_portlet_DownloadsPortlet";
 
-	public static final String ENVIRONMENT = "10_WAR_osblcsportlet";
+	public static final String ENROLLMENT =
+		"com_liferay_osb_lcs_enrollment_portlet_EnrollmentPortlet";
 
-	public static final String INFO = "15_WAR_osblcsportlet";
+	public static final String ENVIRONMENT =
+		"com_liferay_osb_lcs_environment_portlet_EnvironmentPortlet";
 
-	public static final String MEMBERS = "3_WAR_osblcsportlet";
+	public static final String HEADER =
+		"com_liferay_osb_lcs_header_portlet_HeaderPortlet";
 
-	public static final String NAVIGATION = "4_WAR_osblcsportlet";
+	public static final String INFO =
+		"com_liferay_osb_lcs_info_portlet_InfoPortlet";
 
-	public static final String NOTIFICATIONS = "5_WAR_osblcsportlet";
+	public static final String MEMBERS =
+		"com_liferay_osb_lcs_members_portlet_MembersPortlet";
 
-	public static final String SERVER = "11_WAR_osblcsportlet";
+	public static final String NAVIGATION =
+		"com_liferay_osb_lcs_navigation_portlet_NavigationPortlet";
 
-	public static final String SUBSCRIPTIONS = "9_WAR_osblcsportlet";
+	public static final String NOTIFICATIONS =
+		"com_liferay_osb_lcs_notifications_portlet_NotificationsPortlet";
+
+	public static final String SERVER =
+		"com_liferay_osb_lcs_server_portlet_ServerPortlet";
+
+	public static final String SUBSCRIPTIONS =
+		"com_liferay_osb_lcs_subscriptions_portlet_SubscriptionsPortlet";
+
+	public static final String SUPPORT_ADMIN =
+		"com_liferay_osb_lcs_supportadmin_portlet_SupportAdminPortlet";
 
 }
