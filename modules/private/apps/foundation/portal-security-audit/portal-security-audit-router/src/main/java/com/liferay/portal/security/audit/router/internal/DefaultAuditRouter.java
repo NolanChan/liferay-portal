c7a790885341785596e2f7a2ba16bcbdb42d008a
Brian Chan <brian.chan@liferay.com>
2016-09-16 13:21:09 -0700

    LPS-67950 auto SF

--- modules/private/apps/foundation/portal-security-audit/portal-security-audit-router/src/main/java/com/liferay/portal/security/audit/router/internal/DefaultAuditRouter.java
+++ modules/private/apps/foundation/portal-security-audit/portal-security-audit-router/src/main/java/com/liferay/portal/security/audit/router/internal/DefaultAuditRouter.java
@@ -60,7 +60,8 @@ import org.osgi.service.component.annotations.ReferencePolicyOption;
 @Component(
 	configurationPid = "com.liferay.portal.security.audit.configuration.AuditConfiguration",
 	configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true,
-	service = DefaultAuditRouter.class)
+	service = DefaultAuditRouter.class
+)
 public class DefaultAuditRouter implements AuditRouter {
 
 	@Override
