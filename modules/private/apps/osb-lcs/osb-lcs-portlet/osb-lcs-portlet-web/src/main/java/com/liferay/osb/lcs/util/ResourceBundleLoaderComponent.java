e57bc9b0c0c6981bb0549a65721607f284f50cc9
Igor Beslic <igor.beslic@liferay.com>
2016-10-03 12:52:17 +0200

    CLDSVCS-2161 osb-lcs-portlet - new code - add ResourceBundleLoader

---
+++ modules/private/apps/osb-lcs/osb-lcs-portlet/osb-lcs-portlet-web/src/main/java/com/liferay/osb/lcs/util/ResourceBundleLoaderComponent.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.osb.lcs.util;
+
+import com.liferay.portal.kernel.util.AggregateResourceBundleLoader;
+import com.liferay.portal.kernel.util.CacheResourceBundleLoader;
+import com.liferay.portal.kernel.util.ClassResourceBundleLoader;
+import com.liferay.portal.kernel.util.ResourceBundleLoader;
+
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Igor Beslic
+ */
+@Component(immediate = true)
+public class ResourceBundleLoaderComponent implements ResourceBundleLoader {
+
+	@Override
+	public ResourceBundle loadResourceBundle(String languageId) {
+		return _aggregateResourceBundleLoader.loadResourceBundle(languageId);
+	}
+
+	@Reference(target = "(bundle.symbolic.name=com.liferay.osb.lcs.web)", unbind = "-")
+	public void setResourceBundleLoader(
+		ResourceBundleLoader resourceBundleLoader) {
+
+		_aggregateResourceBundleLoader = new AggregateResourceBundleLoader(
+			new CacheResourceBundleLoader(
+				new ClassResourceBundleLoader(
+					"content.Language",
+					ResourceBundleLoaderComponent.class.getClassLoader())),
+			resourceBundleLoader);
+	}
+
+	private AggregateResourceBundleLoader _aggregateResourceBundleLoader;
+
+}
