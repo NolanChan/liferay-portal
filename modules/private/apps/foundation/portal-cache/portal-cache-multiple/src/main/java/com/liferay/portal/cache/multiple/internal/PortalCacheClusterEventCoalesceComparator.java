fd7cffec87a47a1d5890f1f9ce9cd27868c8fdd0
Preston-Crary <preston.crary@liferay.com>
2016-11-16 10:38:21 -0800

    LPS-69078 Compare keys last, we should not compare keys from different caches.

--- modules/private/apps/foundation/portal-cache/portal-cache-multiple/src/main/java/com/liferay/portal/cache/multiple/internal/PortalCacheClusterEventCoalesceComparator.java
+++ modules/private/apps/foundation/portal-cache/portal-cache-multiple/src/main/java/com/liferay/portal/cache/multiple/internal/PortalCacheClusterEventCoalesceComparator.java
@@ -42,15 +42,17 @@ public class PortalCacheClusterEventCoalesceComparator
 			return -1;
 		}
 
+		// Must compare keys last as some cache keys do direct casting.
+
 		if (Objects.equals(
-				portalCacheClusterEvent1.getElementKey(),
-				portalCacheClusterEvent2.getElementKey()) &&
-			Objects.equals(
 				portalCacheClusterEvent1.getPortalCacheManagerName(),
 				portalCacheClusterEvent2.getPortalCacheManagerName()) &&
 			Objects.equals(
 				portalCacheClusterEvent1.getPortalCacheName(),
-				portalCacheClusterEvent2.getPortalCacheName())) {
+				portalCacheClusterEvent2.getPortalCacheName()) &&
+			Objects.equals(
+				portalCacheClusterEvent1.getElementKey(),
+				portalCacheClusterEvent2.getElementKey())) {
 
 			portalCacheClusterEvent1.setElementValue(
 				portalCacheClusterEvent2.getElementValue());
